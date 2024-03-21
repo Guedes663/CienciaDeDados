1ee7a85fd10e4940eade636d0e01a06edb883260
Gabriel Guedes Dos Santos <paulo.guedes.santos7@gmail.com>
2024-03-21 16:50:22 -0300

    Adicionando a atividade de mineracao

--- atividade_kmeans/fonte/atividade.py
+++ atividade_kmeans/fonte/atividade.py
@@ -2,27 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #Carregar o conjunto de dados
-url_do_arquivo = "C:\\Users\\1-22-10794\\Desktop\\atividade\\atividade_kmeans\\csv\\BankChurners.csv"
+url_do_arquivo = "C:\\Users\\gabri\\OneDrive\\Área de Trabalho\\CienciaDeDados\\atividade_kmeans\\csv\\BankChurners.csv"
 
 dados = pd.read_csv(url_do_arquivo)
-#print(dados.describe())
-#print(dados.info())
-#print(dados.head())
+print(dados.describe())
+print(dados.info())
+print(dados.head())
 
 #Verifica valores ausentes
 valores_ausentes = dados.isna().sum()
 
-print(valores_ausentes)
+#print(valores_ausentes)
 
 #Removendo colunas irrelevantes
 dados = dados['Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1', 'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2'].drop()
 
 # Visualizar boxplots para identificar outliers
-plt.figure(figsize=(10, 6))
-dados.boxplot()
-plt.xticks(rotation=90)
-plt.title("Boxplots das colunas numéricas")
-plt.show()
+#plt.figure(figsize=(10, 6))
+#dados.boxplot()
+#plt.xticks(rotation=90)
+#plt.title("Boxplots das colunas numéricas")
+#plt.show()
 
 #CLIENTNUM é o id
 #Attrition_Flag se o cliente é usuário do banco
